372f007eb7a4393b9210ebacff97322f389abd91
BastienGarrigue <64657914+BastienGarrigue@users.noreply.github.com>
2020-10-11 17:25:21 +0200

    Add files via upload

--- slidingWindowRealTime.py
+++ slidingWindowRealTime.py
@@ -15,10 +15,10 @@ from DataExtraction.getRRListAmp import get_rr_list_amp
 from writeData_csv import writeData
 from livePlotter import live_plotter
 from displaySignal import display
-plt.style.use('fivethirtyeight')
+import pickle
+from sklearn.ensemble import RandomForestClassifier
 
 
-    
 def slidingWindowRealTime():
     
     i2c = busio.I2C(board.SCL, board.SDA)
@@ -41,7 +41,7 @@ def slidingWindowRealTime():
     lancement=True
     
     # Window size
-    window_size = 15
+    window_size = 10
     line1=[]
     
     # Starting time of the signal
@@ -55,30 +55,53 @@ def slidingWindowRealTime():
                 time_signal.append(time.time()-temps_actuel)
             lancement=False
     start_time=time_signal[0]
+    time_start_window=time.time()
     while True:
             tension=chan0.value
             amplitude.append(tension)
             time_signal.append(time.time()-temps_actuel)
             #end_time = time_signal[-1]
+            temps_ecoule=time.time()-time_start_window
             #for i in range(round(start_time), round(end_time)-window_size):
-            time_window_30sec = [t for t in time_signal if i <= t <= i+window_size]
-                 
-                # Extract the min and the max of the signal inside the window
-            min_time = min(time_window_30sec)
-            max_time = max(time_window_30sec)
-                # Index of min and max time
-            index_min_time = time_window_30sec.index(min_time)
-            index_max_time = time_window_30sec.index(max_time)
+            
+            if time.time()-time_start_window>=1:
+                time_start_window=time.time()
+                time_window_30sec = [t for t in time_signal if i <= t <= i+window_size]
+                     
+                    # Extract the min and the max of the signal inside the window
+                min_time = min(time_window_30sec)
+                max_time = max(time_window_30sec)
+                
+                    # Index of min and max time
+                index_min_time = time_window_30sec.index(min_time)
+                index_max_time = time_window_30sec.index(max_time)
 
-                # Amplitude of signal inside window
-            amplitude_window_30sec = amplitude[index_min_time:index_max_time]
-            
-            x = get_rr_list_amp(amplitude_window_30sec,time_window_30sec)
-            signal = display(amplitude_window_30sec)
-            BPM_result = BPM(x)
-            #start_time=start_time+1
-            i=i+1
-            
+                    # Amplitude of signal inside window
+                amplitude_window_30sec = amplitude[index_min_time:index_max_time]
+                
+                x = get_rr_list_amp(amplitude_window_30sec,time_window_30sec)
+                signal = display(amplitude_window_30sec)
+                BPM_result = BPM(x)
+                IBI_result = IBI(x)
+                SDNN_result = SDNN(x)
+                SDSD_result = np.std(SDSD(x))
+                RMSSD_result = np.sqrt(np.mean(SDSD(x)))
+                X_pred=[[BPM_result,IBI_result,SDNN_result,SDSD_result,RMSSD_result],]
+                with open('model.pickle','rb') as modelFile:
+                     model = pickle.load(modelFile)
+                pred= model.predict(X_pred)
+                if pred == [1]:
+                    print("L'utilisateur a fait un effort")
+                else:
+                    print("L'utilisateur est au repos")
+                
+                if i>20:
+                    plt.plot(amplitude)
+                    plt.show()
+                i=i+1
+                
+                #start_time=start_time+1
+                   
     print("END")
   
 slidingWindowRealTime()    
